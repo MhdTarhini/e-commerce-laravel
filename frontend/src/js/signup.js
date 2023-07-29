0d49bc0e5eb8c6a5752b549171c93661c267e51d
MhdTarhini <mohamedtarhini95@gmail.com>
2023-07-29 15:15:49 +0300

    check all input validation

--- frontend/src/js/signup.js
+++ frontend/src/js/signup.js
@@ -8,10 +8,13 @@ function singUp() {
   const email = document.getElementById("email");
   const password = document.getElementById("password");
   const repeat_Password = document.getElementById("repeat-password");
-  const interst = document.getElementById("interst");
+  let valid_data = false;
   if (password.value !== repeat_Password.value) {
     password.style.borderColor = "red";
     repeat_Password.style.borderColor = "red";
+  } else {
+    password.style.borderColor = "black";
+    repeat_Password.style.borderColor = "black";
   }
   const sign_up_data = new FormData();
   sign_up_data.append("first-name", first_name.value);
@@ -19,15 +22,40 @@ function singUp() {
   sign_up_data.append("address", address.value);
   sign_up_data.append("email", email.value);
   sign_up_data.append("password", password.value);
-  sign_up_data.append("intersted", interst.value);
 
+  let check_couter = 0;
   for (const [key, value] of sign_up_data.entries()) {
     if (value == "") {
       document.getElementById(`${key}`).style.borderColor = "red";
     } else {
       document.getElementById(`${key}`).style.borderColor = "black";
+      check_couter += 1;
     }
-    console.log(key);
-    console.log(value);
   }
+  if (check_couter == 5) {
+    valid_data = true;
+  }
+  const checkboxes = document.querySelectorAll(
+    'input[type="checkbox"][name="interest"]'
+  );
+  const check_question = document.querySelector(".check-question");
+  const customer_intersted_cat = [];
+  let one_checked = false;
+  checkboxes.forEach((ele) => {
+    if (ele.checked) {
+      customer_intersted_cat.push(ele.value);
+      console.log(customer_intersted_cat);
+      one_checked = true;
+    }
+  });
+  if (!one_checked) {
+    valid_data = false;
+  }
+  if (!one_checked) {
+    check_question.style.color = "red";
+  } else {
+    check_question.style.color = "black";
+  }
+  sign_up_data.append("intersted", customer_intersted_cat);
+  console.log(valid_data);
 }
